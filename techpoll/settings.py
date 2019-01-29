ac321e2f023718da6a39e3db672a50ad3cfe08e8
holyshining <holyshine@yahoo.com>
2019-01-29 17:12:23 +0200

    Add Docker support

--- techpoll/settings.py
+++ techpoll/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v6)knom_9#vtiqo98t!@vpg116k))a#7hy91+g+finrrwp_x0('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'techpoll.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'techpoll',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
