6b7311719c6b16a3dff4cbb70279e49f5bec3b93
holyshining <holyshine@yahoo.com>
2019-02-03 15:43:02 +0200

    Added loading from file

--- actions/urls.py
+++ actions/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('sections/', views.SectionView.as_view(), name='create-section'),
     path('stage/', views.StageView.as_view(), name='create-stage'),
     path('question/', views.QuestionView.as_view(), name='create-question'),
-    path('departments/', views.DepartmentsView.as_view(), name='create-department')
+    path('departments/', views.DepartmentsView.as_view(), name='create-department'),
+    path('load/<mode>', views.LoadFileView.as_view(), name='load-file')
 ]
--- actions/views.py
+++ actions/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 # Create your views here.
-from django.views.generic import TemplateView
 from django.views.generic import View
 from . import models
 from .models import SectionsModel, StagesModel, QuestionsModel
+from .file import file_worker
 
 
 class HomeView(View):
@@ -59,11 +59,74 @@ class QuestionView(View):
 
 class DepartmentsView(View):
     def get(self, request):
-        return render(request, 'actions/create_departments.html', {})
+        questions = QuestionsModel.objects.all()
+        return render(request, 'actions/create_departments.html', {'questions': questions})
 
     def post(self, request):
         department = None
+        print(QuestionsModel.objects.filter(id__in=[1,2]))
+        print(request.POST.getlist('questions'))
         messages.add_message(request,
                              messages.SUCCESS,
                              'Department "{}" created successfully!'.format(request.POST['name']))
+        return redirect('auth-routing')
+
+
+class LoadFileView(View):
+    def get(self, request, mode):
+        return render(request, 'actions/load_from_file.html', {'mode': mode})
+
+    def post(self, request, mode):
+        file = request.FILES['loaded_file']
+        file_context = file_worker(file)
+        query_list = []
+
+        if mode == 'section':
+            for line in file_context:
+                if not SectionsModel.objects.filter(name=line[0]).exists():
+                    query_list.append(SectionsModel(name=line[0]))
+                return redirect('load-file')
+            if query_list:
+                StagesModel.objects.bulk_create(query_list)
+
+        if mode == 'stage':
+            for line in file_context:
+                try:
+                    section_id = SectionsModel.objects.get(name=line[1]).pk
+                    print(line[0], section_id)
+                    if not StagesModel.objects.filter(name=line[0]).exists():
+                        query_list.append(StagesModel(name=line[0], f_section_id=section_id))
+                except SectionsModel.DoesNotExist:
+                    messages.add_message(request,
+                                         messages.ERROR,
+                                         '{} does not exist'.format(line[1]))
+                    return redirect('load-file')
+            if query_list:
+                StagesModel.objects.bulk_create(query_list)
+
+        if mode == 'question':
+            for line in file_context:
+                if len(line) == 3:
+                    name = line[0]
+                    hint = line[1]
+                    stage_id = StagesModel.objects.get(name=line[2]).pk
+                else:
+                    name = line[0]
+                    hint = ""
+                    stage_id = StagesModel.objects.get(name=line[1]).pk
+
+                try:
+                    if not StagesModel.objects.filter(name=name).exists():
+                        query_list.append(QuestionsModel(name=name, hint=hint, f_stage_id=stage_id))
+                except SectionsModel.DoesNotExist:
+                    messages.add_message(request,
+                                         messages.ERROR,
+                                         '{} does not exist'.format(line[1]))
+                    return redirect('load-file')
+            if query_list:
+                QuestionsModel.objects.bulk_create(query_list)
+
+        messages.add_message(request,
+                             messages.SUCCESS,
+                             'File successfully loaded!')
         return redirect('auth-routing')
